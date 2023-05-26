20dc951275e12e9e9c38be0cb1163dc351dbe0d2
mjovanc <mjovanc@icloud.com>
2023-05-26 19:22:06 +0000

    Adding hugin_api_json_rpc_server.cpp

--- src/hugin_api/hugin_api_json_rpc_server.cpp
+++ src/hugin_api/hugin_api_json_rpc_server.cpp
@@ -4,7 +4,5 @@
 
 namespace hugin_api
 {
-    class HuginApi
-    {
-    }
+
 } // namespace hugin_api
--- src/hugin_api/hugin_api_json_rpc_server.h
+++ src/hugin_api/hugin_api_json_rpc_server.h
@@ -2,6 +2,35 @@
 //
 // Please see the included LICENSE file for more information.
 
+#pragma once
+
+#include "common/json_value.h"
 #include "json_rpc_server/json_rpc_server.h"
 #include "serialization/json_input_value_serializer.h"
 #include "serialization/json_output_stream_serializer.h"
+
+namespace hugin_api
+{
+    class HuginApiJsonRpcServer : public cryptonote::JsonRpcServer
+    {
+    public:
+        HuginApiJsonRpcServer(
+            syst::Dispatcher &sys,
+            syst::Event &stop_event,
+            HuginApi &hugin_api,
+            std::shared_ptr<logging::ILogger> logger_group,
+            payment_service::ConfigurationManager &config);
+        HuginApiJsonRpcServer(const HuginApiJsonRpcServer &) = delete;
+
+    protected:
+        virtual void processJsonRpcRequest(const common::JsonValue &req, common::JsonValue &resp) override;
+
+    private:
+        HuginApi &hugin_api;
+        logging::LoggerRef logger;
+
+        typedef std::function<void(const common::JsonValue &json_rpc_params, common::JsonValue &json_response)> HandlerFunction;
+
+        template <typename RequestType, typename ResponseType, typename RequestHandler>
+    }
+} // namespace hugin_api
