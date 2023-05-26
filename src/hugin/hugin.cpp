246903b302bfd8964438eaf453e4c97050a18d7c
mjovanc <mjovanc@icloud.com>
2023-05-26 17:41:54 +0000

    Adding hugin library

---
+++ src/hugin/hugin.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2019, The Kryptokrona Developers
+//
+// Please see the included LICENSE file for more information.
+
+#include "hugin.h"
---
+++ src/hugin/hugin.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, The Kryptokrona Developers
+//
+// Please see the included LICENSE file for more information.
+
+namespace hugin
+{
+
+}
