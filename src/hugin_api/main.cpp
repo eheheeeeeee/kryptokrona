3bc59ff34af8ccb02c47659be6b5e79097291318
mjovanc <mjovanc@icloud.com>
2023-05-26 19:08:29 +0000

    Adding starting point main.cpp for hugin api

---
+++ src/hugin_api/main.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019, The Kryptokrona Developers
+//
+// Please see the included LICENSE file for more information.
+#include <string.h>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::cout << "Starting Hugin API..." << std::endl;
+    return 0;
+}
