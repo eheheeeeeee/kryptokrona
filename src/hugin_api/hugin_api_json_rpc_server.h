0a41843fcfb3b634a27e4700fa10a95e8108a358
mjovanc <mjovanc@icloud.com>
2023-05-26 19:13:57 +0000

    Adding hugin_api_json_rpc_server.h

---
+++ src/hugin_api/hugin_api_json_rpc_server.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2019, The Kryptokrona Developers
+//
+// Please see the included LICENSE file for more information.
+
+#include "json_rpc_server/json_rpc_server.h"
+#include "serialization/json_input_value_serializer.h"
+#include "serialization/json_output_stream_serializer.h"
